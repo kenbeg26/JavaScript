2264573c17fb464378067a7feeaab739d3037b25
kenbegornia2 <kenbegornia2@gmail.com>
2026-05-24 16:48:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Hello world');
+
+//let message = 'Hello'; // define the variable and assign the value
+
+
+
+let age = 25;
+let message = 'Hello';
+
+console.log(message); // Hello!
+
+let admin;
+let name;
+
+name = "John";
+
+admin = name;
+alert(admin);
+
+let planet;
+let user;
