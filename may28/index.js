0bedd70c2317d66b62ef1cece1880871087ab501
kenbegornia2 <kenbegornia2@gmail.com>
2026-05-28 14:39:17 +0800

    add discussion may 28

---
+++ may28/index.js
@@ -0,0 +1,44 @@
+console.log('Lets Go and Thank you Thursday');
+
+// let year = prompt('In which year was ECMAScript-2015 specification published?', '');
+
+// // if (year == 2015) alert('You are right!');
+// if (year < 2015) {
+//   console.log('Too early...');
+// } else if (year > 2015) {
+//   console.log('Too late');
+// } else {
+//   console.log('Exactly!');
+// }
+
+// let accessAllowed;
+// let age = prompt('How old are you?', '');
+
+// if (age > 18) {
+//   accessAllowed = true;
+// } else {
+//   accessAllowed = false;
+// }
+
+// alert(accessAllowed);
+
+// let name = prompt('What is the "official" name of JavaScript?', '');
+// if (name == "ECMAScript") {
+//   alert("Right!")
+// } else {
+//   alert(`You don't know? "ECMAScript"!`);
+// }
+
+// let number = prompt('Enter number?', '');
+
+// if (number > 0) {
+//   alert(1);
+// } else if (number < 0) {
+//   alert(-1)
+// } else if (number == 0) {
+//   alert(0);
+// }
+
+// let result = (a + b < 4) ? 'Below' : 'Over';
+
+alert(null || 2 || undefined);
